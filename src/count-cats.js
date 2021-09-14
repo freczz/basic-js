fcfd478557e2ac0ffad17d39aef733660b41d867
Alexey <alexmikhaylovich@mail.ru>
2021-09-14 19:00:06 +0300

    ВВОд

--- src/count-cats.js
+++ src/count-cats.js
@@ -16,5 +16,9 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function countCats(/* matrix */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let result = 0;
+  for (i = 0; i < countCats.length; i++) {
+    if (countCats[i] === "^^") result++;
+  }
+  return result;
 }
