deda279c629da2e1035b8ceb7c127f487bb169be
Alexey <alexmikhaylovich@mail.ru>
2021-09-15 17:46:48 +0300

    фиксация: "master"

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,5 @@
-import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(backyard) {
   let result = 0;
-  for (i = 0; i < countCats.length; i++) {
-    if (countCats[i] === "^^") result++;
-  }
+  backyard.map(i => { i.forEach(i => { if (i == "^^") result++; }) })
   return result;
 }
