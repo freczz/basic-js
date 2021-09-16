e198704dc94d50e660baa8f73e9dd8d1cbd86531
Alexey <alexmikhaylovich@mail.ru>
2021-09-16 12:28:05 +0300

    win

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,35 @@
-import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
- * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+   if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+   let list = ["--discard-next", "--discard-prev", "--double-next", "--double-prev"];
+   let copyArr = arr.slice();
+   let result = [];
+   for (let i = 0; i < copyArr.length; i++) {
+      if (!(list.includes(copyArr[i]))) {
+         result.push(copyArr[i]);
+         continue;
+      }
+      switch (copyArr[i]) {
+         case "--discard-next":
+            if (typeof copyArr[i + 1] == "number") {
+               copyArr.splice(i + 1, 1);
+            }
+            continue;
+         case "--discard-prev":
+            if (typeof copyArr[i - 1] == "number") {
+               result.pop();
+            }
+            continue;
+         case "--double-next":
+            if (typeof copyArr[i + 1] == "number") {
+               result.push(copyArr[i + 1]);
+            }
+            continue;
+         case "--double-prev":
+            if (typeof copyArr[i - 1] == "number") {
+               result.push(copyArr[i - 1]);
+            }
+            continue;
+      }
+   }
+   return result;
 }
